44295fddc7760894e553539b5f9a9f2fcb602a1f
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2026-02-06 10:41:01 +0530

    Create index.js

---
+++ Video_63/index.js
@@ -0,0 +1,17 @@
+let arr = [1, 3, 5, 7, 9]  
+
+// console.log(arr.push("Ashu"))
+
+// console.log(arr.unshift("aman"))
+
+console.log(arr.splice(1,3))
+
+console.log(arr);
+
+// console.log(arr.length);
+
+// console.log(arr.toString())
+// console.log(arr.join(";"))
+
+// console.log(arr.pop())
+
