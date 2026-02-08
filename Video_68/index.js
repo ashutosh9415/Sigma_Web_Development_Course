c4c272199166047336fafa652b712f20c4b3b22a
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2026-02-08 18:31:00 +0530

    Create index.js

---
+++ Video_68/index.js
@@ -0,0 +1,22 @@
+console.log("JavaScript - Selecting by Ids, Classes, and More")
+
+// let element = document.getElementsByClassName("box")
+// console.log(element)
+
+// element[2].style.backgroundColor = "red"
+// element[3].style.borderRadius = "50%"
+// element[4].style.border = "5px dashed green"
+// element[1].style.fontSize = "15px"
+
+
+// document.getElementById("redBox").style.backgroundColor = "red"
+
+
+// document.querySelector(".box").style.backgroundColor = "red"
+
+let elements = document.querySelectorAll(".box")
+console.log(elements)
+
+document.querySelectorAll(".box").forEach(element => {
+    element.style.backgroundColor = "red"
+})
