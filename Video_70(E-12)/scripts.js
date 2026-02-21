0c5db9a8a380cead7c04ecd33b3a02b61447ce62
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2026-02-21 11:33:53 +0530

    Create scripts.js

---
+++ Video_70(E-12)/scripts.js
@@ -0,0 +1,17 @@
+let first = document.querySelector(".box");
+
+first.style.backgroundColor = "red";
+first.style.color = "yellow";
+first.style.fontSize = "23px";
+
+
+let all = document.getElementsByClassName("box");
+
+all[1].style.backgroundColor = "pink";
+all[1].style.borderRadius = "10px";
+
+all[2].style.backgroundColor = "green"; 
+
+all[3].style.backgroundColor = "yellow";
+
+all[4].style.backgroundColor = "purple";
